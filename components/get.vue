5cc7e5fb34a2039d372c7781563dd80e9b156c15
Maki Myers <maki.myers@jaywing.com>
2022-12-14 14:23:40 +0000

    fetch users test

--- components/get.vue
+++ components/get.vue
@@ -1,8 +1,23 @@
 
 <template>
-    GET POSTS
+
+    <ul>
+        <li v-for="user in users"
+          :key="user">{{ user.name }}</li>
+    </ul>
+    
+   {{ users }}
 </template>
 
+
+
+<script setup>
+const { data: users } = await useFetch('https://gorest.co.in/public/v2/users')
+
+console.log(users)
+</script>
+
+<!--
 <script  >
 export default {
     // fetch data from server/api
@@ -12,7 +27,9 @@ export default {
 
     },
 
-    async mounted() {
+    /* async mounted() {
+
+
         const { data } = await useAsyncData('https://cloud.squidex.io/api/content/garpunkaldev/experience', () => {
             return $fetch('https://cloud.squidex.io/api/content/garpunkaldev/experience', {
                 params: {
@@ -22,9 +39,13 @@ export default {
         })
 
         console.log("DATA", data)
-    }
+    } */ 
+
+
+
+
 
 }
 
 
-</script>
+</script> -->
