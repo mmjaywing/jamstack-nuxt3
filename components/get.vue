a2973b1fcc2cde996824993502d6054db0c5e53b
Maki Myers <maki.myers@jaywing.com>
2022-12-14 14:16:30 +0000

    api test

---
+++ components/get.vue
@@ -0,0 +1,30 @@
+
+<template>
+    GET POSTS
+</template>
+
+<script  >
+export default {
+    // fetch data from server/api
+
+    methods: {
+
+
+    },
+
+    async mounted() {
+        const { data } = await useAsyncData('https://cloud.squidex.io/api/content/garpunkaldev/experience', () => {
+            return $fetch('https://cloud.squidex.io/api/content/garpunkaldev/experience', {
+                params: {
+                    name: 'world'
+                }
+            })
+        })
+
+        console.log("DATA", data)
+    }
+
+}
+
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
     <!-- Nuxt automatically imports any components in your components/ directory -->
     <div>Content in index</div>
+
+    <Get/>
 </template>
 
 <script>
