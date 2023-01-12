8c6142e40d692b819e95201589d3816cf86fceda
Maki Myers <maki.myers@jaywing.com>
2023-01-12 12:27:32 +0000

    footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="w-full bg-[#8959A8] p-8">Footer</div>
+  <div class="w-full bg-[#8959A8] p-8 text-white flex items-center justify-between">
+    <NuxtLink to="/" class="!text-white">
+      Jaywing
+    </NuxtLink>
+    <span class="font-sans text-sm">All rights reserved. © 2023</span>
+  </div>
 </template>
 
 <script>
