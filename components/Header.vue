76cd041eb7f2ca6d46ab1e0af5458fd6e47c342e
Maki Myers <maki.myers@jaywing.com>
2023-01-12 18:39:09 +0000

    icon

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full bg-[#101014] text-white justify-between h-[60px] px-8 items-center gap-8 fixed top-0 z-40">
     <NuxtLink to="/" aria-label="logo">
-      <nuxt-img src="/logo.svg" alt="logo" class="w-8" />
+      <nuxt-img src="/logo.svg" alt="logo" class="w-5" />
     </NuxtLink>
 
     <div class="flex gap-4">
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <template>
-    <div class="bg-hero bg-cover h-hero min-h-[600px] w-full flex relative">
+    <div class="bg-hero bg-cover h-hero min-h-[600px] w-full flex relative" :style="`background: url(${config.API_ASSETS_URL}/${page.data.imageHero.iv}) no-repeat center center`">
         <div class="bg-gradient-to-t from-[#16171D] to-transparent h-hero min-h-[600px] w-full absolute t-0 l-0 z-10 bg-opacity-50"></div>
         <div class="container mx-auto px-4 flex items-end py-8 relative z-20">
-            <div class="text-white flex gap-6 flex-col">
-                <h1 class="text-7xl">Welcome</h1>
-                <p class="text-xl max-w-[600px] serif">Praesent commodo cursus magna, vel scelerisque nisl consectetur
-                    et. Vestibulum id ligula porta felis euismod semper.</p>
+            <div class="text-white flex gap-10 flex-col">
+                <h1 class="text-7xl">{{ page.data.title.iv }}</h1>
+                <p class="text-xl max-w-[600px] serif">{{ page.data.excerpt.iv }}</p>
 
                 <NuxtLink to="/posts/" aria-label="content" class="btn">
                     <span>Wines</span>
@@ -13,6 +12,14 @@
             </div>
         </div>
     </div>
+
+
+    <!-- 
+                <nuxt-img :src="`${config.API_ASSETS_URL}/${page.data.imageHero.iv}`" :alt="page.data.title.iv"
+                        sizes="sm:200px lg:400px" class="w-full h-hero min-h-[600px] object-cover  top-0 left-0" />
+
+    -->
+
 
     <HomePosts />
 
@@ -88,6 +95,8 @@ let clickMe = () => {
     })
 };
 
-
+const config = useRuntimeConfig();
+const { data: page } = await useFetch(`${config.API_BASE_URL}/home/e645cdda-04e7-41f2-8d89-a3e21fd18b25`)
+console.log(page)
 
 </script>
