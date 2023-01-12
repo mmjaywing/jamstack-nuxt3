a06bb1d66d72750cef158546f35bd7f20da62ce0
Maki Myers <maki.myers@jaywing.com>
2023-01-12 18:12:30 +0000

    prog

--- pages/index.vue
+++ pages/index.vue
@@ -14,31 +14,9 @@
         </div>
     </div>
 
-    <div class="bg-[#1F2128] mb-20">
-        <div class="flex flex-col px-8 py-20 gap-6 text-center min-w-full items-center justify-center">
-            <h2 class="text-3xl">Cras mattis consectetur purus sit amet fermentum.</h2>
-        </div>
-        <div class="grid grid-cols-4 px-8 gap-6">
-            <div class="flex flex-col gap-6">
-                <nuxt-img
-                    src="https://images.unsplash.com/photo-1630771496884-46ce7c270a52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
-                    alt="test" sizes="sm:200px lg:400px" class="w-full h-100 object-cover !mb-0" />
-                <h3 class="text-2xl">Fusce Ultricies Vehicula</h3>
-                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam id dolor id nibh ultricies
-                    vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <NuxtLink to="/posts/" aria-label="content" class="btn">
-                    <span>More</span>
-                </NuxtLink>
-            </div>
-            <div>
-                <nuxt-img
-                    src="https://images.unsplash.com/photo-1521512233585-c0bb78faee37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-                    alt="test" sizes="sm:200px lg:400px" class="w-full h-100 object-cover" />
-            </div>
-            <div>ITEM</div>
-            <div>ITEM</div>
-        </div>
-    </div>
+    <HomePosts />
+
+    
 
     <div class="flex h-hero min-h-[600px] bg-[#16171D] ">
         <div class="w-3/4">
