94857ce953ff1d213104cf3dd6d1a94cf6a5a31f
Maki Myers <maki.myers@jaywing.com>
2023-01-12 15:23:06 +0000

    prog

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -33,12 +33,30 @@
         </div>
     </div>
 
-    <div class="flex flex-col py-20">
-        <h3>Nulla vitae elit libero</h3>
-        <p>Donec sed odio dui. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.
+    <div class="flex flex-col py-20 px-4">
+        <div class="container mx-auto  flex flex-col  relative z-20 items-center gap-6">
+        <h3 class="text-2xl">Nulla vitae elit libero</h3>
+        <p class="md:columns-2">Donec sed odio dui. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.
             Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel
             augue laoreet rutrum faucibus dolor auctor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+            auctor. natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
             auctor.</p>
+        </div>
+    </div>
+
+
+
+    <div class="flex flex-col py-20 px-4 bg-white">
+        <div class="container mx-auto  flex flex-col  relative z-20 items-center gap-6">
+        <h3 class="text-2xl">Nulla vitae elit libero</h3>
+        <p class="">Donec sed odio dui. Curabitur blandit tempus porttitor. Vestibulum id ligula porta felis euismod semper.
+            Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+            auctor. natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel
+            augue laoreet rutrum faucibus dolor auctor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+            auctor.</p>
+        </div>
     </div>
 
 
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -19,11 +19,14 @@
 
                 </NuxtLink>
 
-                {{ post.data.title.iv }}
-
-                <button @click="store.hit()">CLICK ME 2</button>
-
-
+                <div class="p-8 flex flex-col gap-6">
+                    <div class="flex flex-row justify-between">
+                        <h3 class="text-2xl text-[#101014]">Nulla vitae elit libero</h3>
+                        <h4 class="text-lg text-[#101014]">£345</h4>
+                    </div>
+                    {{ post.data.title.iv }}
+                    <button @click="store.hit()" class="!bg-[#101014] !text-white btn">Add to basket</button>
+                </div>
             </div>
         </div>
     </div>
