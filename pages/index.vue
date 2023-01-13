8fc628808008ff4f5820fe3860b6888fb4705c76
Maki Myers <maki.myers@jaywing.com>
2023-01-13 09:28:42 +0000

    mobile

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,8 @@
 
         <nuxt-img :src="`${config.API_ASSETS_URL}/${page.data.imageHero.iv}`" alt="test" sizes="sm:200px lg:400px"
             class="w-full h-hero object-cover h-hero min-h-[600px] absolute top-0 left-0" />
-        <div class="bg-gradient-to-t from-[#16171D] to-transparent h-hero min-h-[600px] w-full absolute t-0 l-0 z-10 bg-opacity-50">
+        <div
+            class="bg-gradient-to-t from-[#16171D] to-transparent h-hero min-h-[600px] w-full absolute t-0 l-0 z-10 bg-opacity-50">
         </div>
 
         <div class="container mx-auto px-4 flex items-end py-8 relative z-20">
@@ -22,13 +23,13 @@
 
 
 
-    <div class="flex h-hero min-h-[600px] bg-[#16171D] ">
-        <div class="w-3/4">
+    <div class="flex flex-col md:flex-row md:h-hero min-h-[600px] bg-[#16171D] ">
+        <div class="md:w-3/4">
             <nuxt-img
                 src="https://images.unsplash.com/photo-1521512233585-c0bb78faee37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                 alt="test" sizes="sm:200px lg:400px" class="w-full h-hero object-cover" />
         </div>
-        <div class="w-1/4 flex justify-between flex-col p-8 gap-6">
+        <div class="md:w-1/4 flex justify-between flex-col p-8 gap-6">
             <h3 class="text-4xl">Vestibulum Lorem Commodo</h3>
             <div class="flex flex-col gap-6">
                 <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
@@ -41,13 +42,13 @@
         </div>
     </div>
 
-    <div class="flex h-hero min-h-[600px] bg-[#16171D]">
-        <div class="w-3/4">
+    <div class="flex flex-col md:flex-row md:h-hero min-h-[600px] bg-[#16171D] ">
+        <div class="md:w-3/4">
             <nuxt-img
                 src="https://images.unsplash.com/photo-1521512233585-c0bb78faee37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
                 alt="test" sizes="sm:200px lg:400px" class="w-full h-hero object-cover" />
         </div>
-        <div class="w-1/4 flex justify-between flex-col  gap-6">
+        <div class="md:w-1/4 flex justify-between flex-col gap-6">
             <nuxt-img
                 src="https://images.unsplash.com/photo-1630771496884-46ce7c270a52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
                 alt="test" sizes="sm:200px lg:400px" class="w-full h-100 object-cover" />
