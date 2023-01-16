d1d1d4a0b1d71f0a9a7cee61ab015a50c6fc05db
Maki Myers <maki.myers@jaywing.com>
2023-01-16 11:38:46 +0000

    nice style

--- components/HeroBlockColor.vue
+++ components/HeroBlockColor.vue
@@ -2,7 +2,7 @@
     <div class="bg-[#A6307F] h-hero min-h-[600px] w-full flex ">
         <div class="container mx-auto px-4 flex items-end py-8">
             <div class="text-white flex gap-6 flex-col">
-                <nuxt-img src="/logo.svg" alt="logo" class="w-48 mb-8" />
+                <nuxt-img src="/logo.svg" alt="logo" class="w-24 md:w-48 mb-8" />
                 <h1 class="text-5xl md:text-7xl">{{ title }}</h1>
 
                 <div v-html="excerpt" class="text-xl max-w-[600px] serif"></div>
